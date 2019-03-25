87d449dc9e6c0403d7225e3fd7911176b7138397
kitagawa-hr <kitagawa@cancerscan.jp>
2019-03-25 22:10:50 +0900

    Create work_log.py

---
+++ test_work_log.py
@@ -0,0 +1,57 @@
+import unittest
+from datetime import datetime
+
+import work_log
+
+
+class TestWorklog(unittest.TestCase):
+    def test_last_day_of_month(self):
+        test_cases = [(2019, 1, 31), (2019, 3, 31), (2019, 2, 28), (2020, 2, 29)]
+        for year, month, day in test_cases:
+            with self.subTest(year=year, month=month, day=day):
+                self.assertEqual(work_log.last_day_of_month(year, month), day)
+
+    def test_take_if_exists(self):
+        test_cases = [
+            ([1, 2, 3], 0, 1),
+            ([1, 2, 3], 1, 2),
+            ([1, 2, 3], -1, 3),
+            ([], 1, None),
+            ([1, 2, 3], 10, None),
+            ([], 0, None),
+            ([1, 3, 5], 4, None),
+        ]
+        for lis, index, element in test_cases:
+            with self.subTest(lis=lis, index=index, element=element):
+                self.assertEqual(work_log.take_if_exists(lis, index), element)
+
+    def test_minmax_datetime(self):
+        test_cases = [
+            (min, [datetime(2019, 1, 1), datetime(2019, 1, 1)], datetime(2019, 1, 1)),
+            (max, [datetime(2019, 1, 1), datetime(2019, 1, 1)], datetime(2019, 1, 1)),
+            (min, [datetime(2019, 1, 1), datetime(2019, 1, 31)], datetime(2019, 1, 1)),
+            (max, [datetime(2019, 1, 1), datetime(2019, 1, 31)], datetime(2019, 1, 31)),
+            (
+                min,
+                [datetime(2019, 1, 1), datetime(2019, 1, 1), datetime(2018, 12, 12)],
+                datetime(2018, 12, 12),
+            ),
+            (max, [datetime(2019, 1, 1), None], datetime(2019, 1, 1)),
+            (max, [None, None], None),
+            (max, [], None),
+        ]
+        for min_or_max, dates, date in test_cases:
+            with self.subTest(min_or_max=min_or_max, dates=dates, date=date):
+                self.assertEqual(work_log.minmax_datetime(min_or_max, *dates), date)
+
+    def test_format_datetime(self):
+        test_cases = [
+            (datetime(2019, 10, 10, 12, 24), "12:24"),
+            (datetime(2019, 10, 10, 1, 24), "1:24"),
+            (datetime(2019, 10, 10, 3, 24), "3:24"),
+            (datetime(2019, 10, 10, 0, 24), "0:24"),
+            (None, None),
+        ]
+        for dt, formatted_dt in test_cases:
+            with self.subTest(dt=dt, formatted_dt=formatted_dt):
+                self.assertEqual(work_log.format_datetime(dt), formatted_dt)
---
+++ work_log.py
@@ -0,0 +1,146 @@
+import calendar
+import datetime
+import subprocess
+
+import pandas as pd
+
+
+WEEKDAY = "月火水木金土日"
+
+
+def get_sleep_logs(year, month):
+    if month < 10:
+        month_str = "0" + str(month)
+    else:
+        month_str = str(month)
+    p1 = subprocess.Popen(["pmset", "-g", "log"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(
+        ["grep", "Kernel Idle sleep preventers"],
+        stdin=p1.stdout,
+        stdout=subprocess.PIPE,
+    )
+    p3 = subprocess.Popen(
+        ["grep", f"{year}-{month_str}-"], stdin=p2.stdout, stdout=subprocess.PIPE
+    )
+    p4 = subprocess.Popen(
+        ["awk", "{print $1, $2}"], stdin=p3.stdout, stdout=subprocess.PIPE
+    )
+    p1.stdout.close()
+    p2.stdout.close()
+    p3.stdout.close()
+    output = p4.communicate()[0]
+    sleep_logs_str = output.decode("utf-8").split("\n")
+    sleep_logs = [
+        datetime.datetime.strptime(log, "%Y-%m-%d %H:%M:%S")
+        for log in sleep_logs_str
+        if log
+    ]
+    return sleep_logs
+
+
+def get_loginout_log(year, month, in_or_out="in"):
+    month_en = calendar.month_abbr[month]
+    if in_or_out == "in":
+        arg = "reboot"
+    else:
+        arg = "shutdown"
+
+    p1 = subprocess.Popen(["last", arg], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep", month_en], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p3 = subprocess.Popen(
+        ["awk", "{print $5, $6}"], stdin=p2.stdout, stdout=subprocess.PIPE
+    )
+    output = p3.communicate()[0]
+    logs_str = output.decode("utf-8").split("\n")
+    logs = [
+        datetime.datetime.strptime(f"{year}-{month}-" + log, "%Y-%m-%d %H:%M")
+        for log in logs_str
+        if log
+    ]
+    return logs
+
+
+def last_day_of_month(year, month):
+    next_month = datetime.datetime(year, month + 1, 1)
+    return (next_month - datetime.timedelta(days=next_month.day)).day
+
+
+def take_if_exists(lis, index):
+    try:
+        return lis[index]
+    except IndexError:
+        return None
+
+
+def minmax_datetime(min_or_max, *dates):
+    try:
+        return min_or_max([date for date in dates if date])
+    except ValueError:
+        return None
+
+
+def make_summary(year, month):
+    login_logs = get_loginout_log(year, month, "in")
+    logout_logs = get_loginout_log(year, month, "out")
+    sleep_logs = get_sleep_logs(year, month)
+    date_range = pd.date_range(
+        datetime.datetime(year, month, 1),
+        datetime.datetime(year, month, last_day_of_month(year, month)),
+    )
+    login_first = {
+        date: take_if_exists(
+            [log for log in login_logs if log.day == date.date().day], 0
+        )
+        for date in date_range
+    }
+    logout_last = {
+        date: take_if_exists(
+            [log for log in logout_logs if log.day == date.date().day], -1
+        )
+        for date in date_range
+    }
+    sleep_first = {
+        date: take_if_exists(
+            [log for log in sleep_logs if log.day == date.date().day], 0
+        )
+        for date in date_range
+    }
+    sleep_last = {
+        date: take_if_exists(
+            [log for log in sleep_logs if log.day == date.date().day], -1
+        )
+        for date in date_range
+    }
+    summary = pd.DataFrame({"date": date_range})
+    summary["login_first"] = login_first.values()
+    summary["logout_last"] = logout_last.values()
+    summary["sleep_first"] = sleep_first.values()
+    summary["sleep_last"] = sleep_last.values()
+    summary["start"] = [
+        minmax_datetime(min, a, b)
+        for a, b in zip(summary["login_first"], summary["sleep_first"])
+    ]
+    summary["end"] = [
+        minmax_datetime(max, a, b)
+        for a, b in zip(summary["logout_last"], summary["sleep_last"])
+    ]
+    return summary
+
+
+def format_datetime(dt):
+    if not dt or dt != dt:
+        return None
+    return f"{int(dt.hour)}:{int(dt.minute)}"
+
+
+if __name__ == "__main__":
+    year = input("year:")
+    month = input("month:")
+    summary = make_summary(2019, 3)
+    formatted = pd.DataFrame()
+    formatted["月"] = month
+    formatted["日"] = summary["date"].dt.day
+    formatted["曜日"] = summary["date"].dt.date.apply(lambda x: WEEKDAY[x.weekday()])
+    formatted["勤務開始時間"] = summary["start"].apply(format_datetime)
+    formatted["勤務終了時間"] = summary["end"].apply(format_datetime)
+    formatted.to_csv(f"{year}年{month}月.csv", index=False, encoding="cp932", mode="a")
